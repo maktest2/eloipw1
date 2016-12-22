4954e16e654514af4af0c6e04306a7d0011621bf
milan <milan@srpski.biz>
2016-12-22 18:29:46 +0000

    Add Latin tests.

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-var test = require('tape');
-var lib = require('..');
-
-test('basic', function (assert) {
-  assert.plan(1);
-  assert.equal(lib.toLatin( 'Б' ), 'B', 'our sample returned correctly');
-});
